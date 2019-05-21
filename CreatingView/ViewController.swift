49acf1d4cb5645bb71be52c410dc3c2fe2c8c163
stven1004 <stven100482@gmail.com>
2019-05-21 12:47:47 +0900

    add view

--- CreatingView/ViewController.swift
+++ CreatingView/ViewController.swift
@@ -12,7 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let frame = CGRect(x: 50, y: 50, width: 100, height: 100)
+        
+        let v = UIView(frame: frame)
+        
+        v.backgroundColor = UIColor.blue
+        
+        view.addSubview(v)
     }
 
 
